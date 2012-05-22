f7a9d36002f9b58e0ac010c95797785d49cd6a54
Aldo Sarmiento <sarmiena@gmail.com>
2012-05-22 12:55:37 -0700

    setting up capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,56 @@
+require 'bundler/capistrano'
+default_run_options[:pty] = true
+
+set :application, "test_app"
+set :user, "deployer1"
+set :deploy_to, "/var/www/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :scm, :git
+set :repository,  "git@github.com:bpd-deployer/test_app.git"
+set :branch, "master"
+
+server "10.25.250.110", :web, :app, :db, primary: true                
+server "10.25.250.111", :web, :app                              
+
+after "deploy", "deploy:cleanup" # only keep the last 5 releases
+
+task :hello do
+  run "echo 'hello world' > ~/hello"
+end
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  task :setup_config, roles: :app do
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
+end
